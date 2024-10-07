5d11a6c9244ebe4bf6382ba7e3adc5be37782733
Faisal Hanif <mehrfaisal111@example.com>
2024-10-07 11:05:52 -0700

    new functionality has been added

--- android/app/src/main/java/com/techxelo/dairyapp/MainActivity.java
+++ android/app/src/main/java/com/techxelo/dairyapp/MainActivity.java
@@ -1,5 +1,15 @@
 package com.techxelo.dairyapp;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.notifications.notifications.LocalNotifications;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize Local Notifications
+        LocalNotifications.initialize(this);
+    }
+}
